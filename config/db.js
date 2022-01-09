6c87197172e11508882876482763ee59e1cd270a
hrefdev <href.dev@gmail.com>
2022-01-09 10:48:47 +0700

    db heroku

--- config/db.js
+++ config/db.js
@@ -1,9 +1,14 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("auth", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-  timezone: "+07:00",
-});
+const db = new Sequelize(
+  "heroku_d867145d301a970",
+  "b9a2f057d1b68a",
+  "1508f860",
+  {
+    host: "us-cdbr-east-05.cleardb.net",
+    dialect: "mysql",
+    timezone: "+07:00",
+  }
+);
 
 export default db;
