e7fa5c8a2b6e493cede6bbae3085ae66a63ac457
hrefdev <href.dev@gmail.com>
2022-01-13 02:52:12 +0700

    db test buntu

--- config/db.js
+++ config/db.js
@@ -8,17 +8,26 @@
 
 // module.exports = db;
 
+// const { Sequelize } = require("sequelize");
+
+// const db = new Sequelize(
+//   "heroku_d867145d301a970",
+//   "b9a2f057d1b68a",
+//   "1508f860",
+//   {
+//     host: "us-cdbr-east-05.cleardb.net",
+//     dialect: "mysql",
+//     timezone: "+07:00",
+//   }
+// );
+
+module.exports = db;
 const { Sequelize } = require("sequelize");
 
-const db = new Sequelize(
-  "heroku_d867145d301a970",
-  "b9a2f057d1b68a",
-  "1508f860",
-  {
-    host: "us-cdbr-east-05.cleardb.net",
-    dialect: "mysql",
-    timezone: "+07:00",
-  }
-);
+const db = new Sequelize("auth", "root", "Jarumcoklat.123", {
+  host: "localhost",
+  dialect: "mysql",
+  timezone: "+07:00",
+});
 
 module.exports = db;
