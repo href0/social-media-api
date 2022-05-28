1bf86e18adb7d1c50729309288199d47da2f46a3
hrefdev <href.dev@gmail.com>
2022-05-28 14:15:10 +0700

    update timeline

--- controllers/Post.js
+++ controllers/Post.js
@@ -356,8 +356,8 @@ const timeline = async (req, res) => {
               ],
             },
           ],
-          offset: req.body.offset ? req.body.offset : 0,
-          limit: req.body.limit ? req.body.limit : 5,
+          offset: Number(req.params.offset) ? Number(req.params.offset) : 0,
+          limit: Number(req.params.limit) ? Number(req.params.limit) : 5,
           order: [["updatedAt", "DESC"]],
         });
       })
